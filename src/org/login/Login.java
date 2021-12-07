feae2979f58f29006abb1e75196fef2d49c2f22f
Rajashri-A <rajashri0404@gmail.com>
2021-12-07 17:31:49 +0530

    Password Functionality Completed

--- src/org/login/Login.java
+++ src/org/login/Login.java
@@ -1,5 +1,15 @@
 package org.login;
 
 public class Login {
+	
+	private static void passwordName() {
+		System.out.println("PasswordName Functionality");
+		
+		}
+	public static void main(String[] args) {
+		passwordName();
+	}
+
+   
 
 }
