d2df06563ef6f8750747f6e41117a8e460ccd1d8
Rajashri-A <rajashri0404@gmail.com>
2021-12-07 17:10:38 +0530

    userName Functionality Completed

--- src/org/login/Login.java
+++ src/org/login/Login.java
@@ -1,5 +1,15 @@
 package org.login;
 
 public class Login {
+	
+	public static void userName() {
+		System.out.println("username Functionality");
+
+	}
+	public static void main(String[] args) {
+		userName();
+	}
+	
+	
 
 }
